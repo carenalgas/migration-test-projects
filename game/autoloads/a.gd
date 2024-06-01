6f76ede5c228ed554a6f73fe72ab976dce5c3182
Carenalga <mapedorr@gmail.com>
2024-06-01 05:05:46 -0500

    Short game made in Popochiu 2 - Beta 3

---
+++ game/autoloads/a.gd
@@ -0,0 +1,22 @@
+@tool
+extends "res://addons/popochiu/engine/interfaces/i_audio.gd"
+
+# cues ----
+var mx_two_popochius: AudioCueMusic = load("res://game/rooms/house/audio/mx_two_popochius.tres")
+var vo_goddiu_normal_01: AudioCueSound = load("res://game/characters/goddiu/audio/vo_goddiu_normal_01.tres")
+var vo_goddiu_normal_02: AudioCueSound = load("res://game/characters/goddiu/audio/vo_goddiu_normal_02.tres")
+var vo_goddiu_normal_03: AudioCueSound = load("res://game/characters/goddiu/audio/vo_goddiu_normal_03.tres")
+var sfx_apple_fall: AudioCueSound = load("res://game/rooms/outside/props/tree/sfx_apple_fall.tres")
+var sfx_tree_impact: AudioCueSound = load("res://game/rooms/outside/props/tree/sfx_tree_impact.tres")
+var sfx_door_open: AudioCueSound = load("res://game/rooms/house/props/door/sfx_door_open.tres")
+var vo_popsy_happy_01: AudioCueSound = load("res://game/characters/popsy/audio/vo_popsy_happy_01.tres")
+var vo_popsy_happy_02: AudioCueSound = load("res://game/characters/popsy/audio/vo_popsy_happy_02.tres")
+var vo_popsy_happy_03: AudioCueSound = load("res://game/characters/popsy/audio/vo_popsy_happy_03.tres")
+var vo_popsy_normal_04: AudioCueSound = load("res://game/characters/popsy/audio/vo_popsy_normal_04.tres")
+var vo_popsy_normal_05: AudioCueSound = load("res://game/characters/popsy/audio/vo_popsy_normal_05.tres")
+var vo_popsy_normal_06: AudioCueSound = load("res://game/characters/popsy/audio/vo_popsy_normal_06.tres")
+var vo_popsy_sad_04: AudioCueSound = load("res://game/characters/popsy/audio/vo_popsy_sad_04.tres")
+var vo_popsy_sad_05: AudioCueSound = load("res://game/characters/popsy/audio/vo_popsy_sad_05.tres")
+var vo_popsy_sad_06: AudioCueSound = load("res://game/characters/popsy/audio/vo_popsy_sad_06.tres")
+# ---- cues
+
